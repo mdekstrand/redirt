8df11b0da1a000391e248c0f3068709e6186b23e
Michael Ekstrand <md@ekstrandom.net>
2023-10-08 13:51:21 -0400

    Correct how logging works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
-use happylog::clap::LogOpts;
 use log::*;
 
 mod commands;
@@ -11,17 +10,38 @@ use commands::{Command, DirCommands};
 /// Recursive directory tool.
 #[derive(Parser, Debug)]
 struct RDTCLI {
-    #[command(flatten)]
-    logging: LogOpts,
+    /// Suppress informational output
+    #[arg(short = 'q', long = "quiet")]
+    quiet: bool,
+    /// Increase logging verbosity (can be repeated)
+    #[arg(short='v', long="verbose", action=clap::ArgAction::Count)]
+    verbose: u8,
 
     #[command(subcommand)]
     command: DirCommands,
 }
 
+impl RDTCLI {
+    fn init_logging(&self) -> Result<()> {
+        let mut verbose: usize = 2;
+        if self.verbose > 0 {
+            verbose += self.verbose as usize;
+        } else if self.quiet {
+            verbose -= 1;
+        }
+
+        stderrlog::new()
+            .module(module_path!())
+            .verbosity(verbose)
+            .init()?;
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
     let cli = RDTCLI::parse();
-    cli.logging.init()?;
+    cli.init_logging()?;
     debug!("starting rdt");
-    cli.command.run();
+    cli.command.run()?;
     Ok(())
 }
